295c96b6915374fdb19c6e487af5851d9eef3eb1
JOYE LIN <y12studio@gmail.com>
2018-04-08 14:43:26 +0800

    add event tests

--- rtob-api/test/app.e2e-spec.ts
+++ rtob-api/test/app.e2e-spec.ts
@@ -13,3 +13,20 @@ test('It should response 404', async () => {
   const response = await GetReq('/util/api-not-exist');
   expect(response.statusCode).toBe(404);
 });
+
+test('/event, new event, non-auth, 201 created', async () => {
+  const event = new EventEntity();
+  // "D4": {"problems": {"1": {"signs": {"11": {"cht": "高血糖", "eng": "hyperglycemia"}, 
+  event.omahaRaw = '{"D4":"TODO"}';
+  event.pcsDomain = 'D4';
+  event.pcsProblem = 1;
+  event.pcsSign = 11;
+  const response = await PostReq('/event', event);
+  expect(response.statusCode).toBe(HttpStatus.CREATED);
+});
+
+test('/event/events, non-auth , 200 ok', async () => {
+  const response = await GetReq('/event/events');
+  expect(response.statusCode).toBe(200);
+  console.log(response.body);
+});
