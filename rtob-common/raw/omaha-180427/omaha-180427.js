b1f63ffa0c0fe7b286acd7944536de3192c8279e
y12studio <y12studio@gmail.com>
2018-04-28 21:58:48 +0800

    update omaha-180427

--- rtob-common/raw/omaha-180427/omaha-180427.js
+++ rtob-common/raw/omaha-180427/omaha-180427.js
@@ -12,11 +12,33 @@ var options = {
   quote: '"' // optional
 };
 
-var x = csvjson
-  .toObject(data, options)
+var problem = csvjson
+  .toObject(data, options).map(v=>{
+    const r = {}
+    r.problem = v["問題類型"]
+    r.problemEn = v["英文原文"]
+    r.domain = v["面向名稱"]
+    r.code = v["代碼"]
+    return r;
+  }).reduce((res,obj)=>{
+    // group by domain
+    const domainArr = obj.domain.split(".")
+    const domain = domainArr[0]
+    res[domain] = res[domain] || [];
+    obj.domain = domainArr[1]
+    obj.problem = obj.problem.substring(3)
+    obj.signs = ['TODO']
+    res[domain].push(obj);
+    return res;
+  },{})
+
+
+const result = {
+  domain : problem
+}
 
 fs.writeFileSync(
   path.join(__dirname, "omaha-180427.json"),
-  JSON.stringify(x, null, 4),
+  JSON.stringify(result, null, 4),
   { encoding: "utf8" }
 );
